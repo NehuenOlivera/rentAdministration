096c24b13126296b926c6a8cfeef84216c164fc3
Nehuen Olivera <nehuenolivera@gmail.com>
2024-09-06 16:38:39 -0300

    Login and logout

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const PropertyFormSchema = z.object({
     id: z.string(),
@@ -280,4 +282,21 @@ export type State = {
         comments?: string[];
     };
     message?: string | null;
+}
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Credenciales inválidas';
+                default:
+                    return 'Algo salió mal';
+            }
+        }
+        throw error
+    }
 }
