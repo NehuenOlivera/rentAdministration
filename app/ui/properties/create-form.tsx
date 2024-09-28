0b784adb41b947a251fa961b9c59b990b33d41b4
Nehuen Olivera <nehuenolivera@gmail.com>
2024-09-27 22:36:04 -0300

    No bank account required when creating Property

--- app/ui/properties/create-form.tsx
+++ app/ui/properties/create-form.tsx
@@ -73,7 +73,7 @@ export default function Form({ frequencies, bankAccounts }: { frequencies: Frequ
                         className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                         required
                     />
-                    <Bars2Icon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                    <MapPinIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
                 </div>
 
                 <div className="relative lg:col-span-1">
@@ -384,7 +384,6 @@ export default function Form({ frequencies, bankAccounts }: { frequencies: Frequ
                 className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 defaultValue=""
                 aria-describedby='adjustment-frequency-id-error'
-                required
               >
                 <option value="" disabled>
                   Cuenta
--- app/ui/properties/edit-form.tsx
+++ app/ui/properties/edit-form.tsx
@@ -79,7 +79,7 @@ export default function EditPropertyForm({
                         className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                         required
                     />
-                    <Bars2Icon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                    <MapPinIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
                 </div>
 
                 <div className="relative lg:col-span-1">
